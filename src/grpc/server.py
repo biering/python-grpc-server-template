7221a90723ee347175b738ea23d77b785f84f614
Christoph Biering <christoph.biering@wandelbots.com>
2019-12-16 11:04:46 +0100

    improved server

--- src/grpc/server.py
+++ src/grpc/server.py
@@ -4,20 +4,29 @@ import grpc
 
 
 class GRPCServer(object):
+
+    @property
+    def instance(self):
+        return self.__server
+
     def __init__(self, address='[::]', port=50051):
-        self.address = address
-        self.port = port
-        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        self.__address = address
+        self.__port = port
+        self.__server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
     def serve(self):
-        endpoint = f'{self.address}:{str(self.port)}'
+        endpoint = f'{self.__address}:{str(self.__port)}'
 
         print(f'Started GRPC Server at {endpoint}')
         print('Serving...')
 
-        self.server.add_insecure_port(endpoint)
-        self.server.start()
-        self.server.wait_for_termination()
+        self.__server.add_insecure_port(endpoint)
+        self.__server.start()
+        self.__server.wait_for_termination()
+
+    def stop(self):
+        print("Stopping GRPC Server gracefully")
+        self.__server.stop(3)
 
 
 if __name__ == '__main__':
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import logging
+import signal
+import sys
 
 from .grpc.server import GRPCServer
 from .proto import helloworld_pb2_grpc
@@ -9,9 +11,17 @@ def add_services(server):
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
 
 
+def signalHandler(signal, frame):
+    print('Process Interrupted!\n\a')
+    server.stop()
+    sys.exit(0)
+
+
 if __name__ == '__main__':
     logging.basicConfig()
 
-    server = GRPCServer()
-    add_services(server.server)
+    server = GRPCServer(address='0.0.0.0', port=4242)
+    signal.signal(signal.SIGINT, signalHandler)
+
+    addServices(server.instance)
     server.serve()
